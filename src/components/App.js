0f64edcc2c0c44c4bbd0fadd9d5c57e704538867
Andrew Chung <andrewchung2022@u.northwestern.edu>
2019-10-06 01:37:14 +0900

    task adding function

--- src/components/App.js
+++ src/components/App.js
@@ -7,14 +7,45 @@ import { Layout } from "antd";
 const { Content } = Layout;
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDetails: false,
+      tasksInProg: [],
+      tasksCompleted: []
+    };
+    this.addTask = this.addTask.bind(this);
+    this.completeTask = this.completeTask.bind(this);
+  }
+
+  addTask(name) {
+    if (name.length === 0) return;
+    let tasks = [...this.state.tasksInProg];
+    tasks.push({ name, time: "Undated" });
+    this.setState({ tasksInProg: tasks });
+    console.log(this.state.tasksInProg);
+  }
+
+  completeTask(task) {
+    let taskIndex = this.state.tasks.indexOf(task);
+    let inProgress = [...this.state.tasksInProg];
+    let completed = [...this.state.tasksCompleted];
+    inProgress.splice(taskIndex, 1);
+    completed.push(task);
+    this.setState({
+      tasksInProg: inProgress,
+      tasksCompleted: completed
+    });
+  }
+
   render() {
     return (
       <Layout style={{ height: "100vh" }}>
         <Nav />
         <Layout>
           <Content style={{ margin: "16px" }}>
-            <TaskList />
-            <TaskDetails />
+            <TaskList addTask={this.addTask} tasks={this.state.tasksInProg} />
+            {this.showDetails && <TaskDetails />}
           </Content>
         </Layout>
       </Layout>
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,28 +2,45 @@ import React from "react";
 import Task from "./Task";
 import { Col, Input, Button } from "antd";
 
-export default function TaskList(props) {
-  return (
-    <Col
-      span={12}
-      style={{
-        backgroundColor: "white",
-        height: "100%",
-        padding: "16px",
-        overflow: "auto"
-      }}
-    >
-      <div className="task-adder">
-        <div className="task-input">
-          <Input placeholder="New Task" />
-        </div>
-        <div className="add-task-button">
-          <Button type="primary" shape="circle" icon="plus" />
-        </div>
-      </div>
+export default class TaskList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { newTask: "" };
+    this.changeNewTask = this.changeNewTask.bind(this);
+  }
 
-      <Task name="name 1" />
-      <Task name="name 2" />
-    </Col>
-  );
+  changeNewTask(e) {
+    this.setState({ newTask: e.target.value });
+  }
+
+  render() {
+    return (
+      <Col
+        span={12}
+        style={{
+          backgroundColor: "white",
+          height: "100%",
+          padding: "16px",
+          overflow: "auto"
+        }}
+      >
+        <div className="task-adder">
+          <div className="task-input">
+            <Input placeholder="New Task" onChange={this.changeNewTask} />
+          </div>
+          <div className="add-task-button">
+            <Button
+              type="primary"
+              shape="circle"
+              icon="plus"
+              onClick={() => this.props.addTask(this.state.newTask)}
+            />
+          </div>
+        </div>
+        {this.props.tasks.map(task => (
+          <Task name={task.name} />
+        ))}
+      </Col>
+    );
+  }
 }
